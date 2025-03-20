7136dc59976f81ba5c02aaa9859cd283c8f91c64
Jay <hsong45@my.centennialcollege.ca>
2025-03-20 13:43:29 -0400

    add signin function with next-auth

---
+++ src/app/lib/ai.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+import OpenAi from "openai";
+
+type Information = {
+  name: string;
+  birth_date: string;
+  birth_time: string;
+  gender: string;
+  city_country: string;
+};
+
+const client = new OpenAi({ apiKey: process.env.OPENAI_API_KEY });
+
+export const getZodiac = async ({
+  name,
+  birth_date,
+  birth_time,
+  gender,
+  city_country,
+}: Information) => {
+  const prompt = `Based on the following birth details, return only the astrological zodiac sign (Sun sign) without any additional explanation or text.\n\n{name: ${name}, birth Date: ${birth_date}, birth time: ${birth_time}, born city: ${city_country}, gender: ${gender}}`;
+
+  try {
+    const completion = await client.chat.completions.create({
+      model: "gpt-4o",
+      messages: [{ role: "user", content: prompt }],
+    });
+
+    return completion.choices[0].message.content;
+  } catch (error) {
+    console.error("❌ Error fetching zodiac sign:", error);
+    return "";
+  }
+};
