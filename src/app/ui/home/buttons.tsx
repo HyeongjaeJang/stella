d0257e596c97a7cad2929eb937217166cfea201b
Jay <hsong45@my.centennialcollege.ca>
2025-03-27 10:36:23 -0400

    add icons on the buttons

--- src/app/ui/home/buttons.tsx
+++ src/app/ui/home/buttons.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import Image from "next/image";
 
+const images = [
+  "/work.png",
+  "/health.png",
+  "/mood.png",
+  "/health.png",
+  "/mood.png",
+];
 const buttons = ["Work", "People", "Finance", "Health", "Mood"];
 
 const Buttons = () => {
   return (
     <div className="w-full p-5">
-      <div className="flex justify-around overflow-auto gap-2">
-        {buttons.map((button) => (
-          <div
-            key={button}
-            className="px-7 py-5 rounded-md bg-white shadow-neutral-500 shadow-inner"
-          >
-            <p className="text-button text-[1rem] font-bold">{button}</p>
+      <div className="flex justify-between overflow-auto gap-4">
+        {buttons.map((button, i) => (
+          <div key={button} className="flex flex-col justify-center">
+            <Image src={images[i]} alt={button} width={200} height={200} />
+            <p className="text-button text-[1rem] font-bold text-center">
+              {button}
+            </p>
           </div>
         ))}
       </div>
--- src/app/ui/home/header.tsx
+++ src/app/ui/home/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="flex justify-between w-full p-2">
       <Image src="/Stella.png" alt="logo" width={80} height={80} />
       <div className="w-1/6">
-        <p className="text-center bg-white p-3 rounded-xxl mt-4 mr-2 font-bold text-sm leading-6">
+        <p className="text-center bg-white p-2 py-3 rounded-xxl mt-4 mr-2 font-bold text-sm leading-6">
           Jay
         </p>
       </div>
